ade7f1160e8994afab5c279cb1fc5f5bc3ddaa58
Movie Recommender System <dev@movierec.local>
2026-06-26 00:56:36 +0530

    Add movie descriptions from TMDB API

--- app.py
+++ app.py
@@ -2,27 +2,32 @@ import pickle
 import streamlit as st
 import requests
 
-def fetch_poster(movie_id):
+def fetch_movie_details(movie_id):
     url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
     data = requests.get(url)
     data = data.json()
-    poster_path = data['poster_path']
-    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
-    return full_path
+    poster_path = data.get('poster_path')
+    if poster_path:
+        full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+    else:
+        full_path = "https://via.placeholder.com/500x750?text=No+Poster"
+    return full_path, data.get('overview', 'No description available.')
 
 def recommend(movie):
     index = movies[movies['title'] == movie].index[0]
     distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
     recommended_movie_names = []
     recommended_movie_posters = []
+    recommended_movie_overviews = []
     for i in distances[1:6]:
-        # fetch the movie poster
+        # fetch the movie poster and details
         movie_id = movies.iloc[i[0]].movie_id
-        recommended_movie_posters.append(fetch_poster(movie_id))
+        poster, overview = fetch_movie_details(movie_id)
+        recommended_movie_posters.append(poster)
         recommended_movie_names.append(movies.iloc[i[0]].title)
+        recommended_movie_overviews.append(overview)
 
-    return recommended_movie_names,recommended_movie_posters
-
+    return recommended_movie_names, recommended_movie_posters, recommended_movie_overviews
 
 st.header('Movie Recommender System')
 movies = pickle.load(open('movies.pkl', 'rb'))
@@ -35,25 +40,12 @@ selected_movie = st.selectbox(
 )
 
 if st.button('Show Recommendation'):
-    recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
-    col1, col2, col3, col4, col5 = st.columns(5)
-    with col1:
-        st.text(recommended_movie_names[0])
-        st.image(recommended_movie_posters[0])
-    with col2:
-        st.text(recommended_movie_names[1])
-        st.image(recommended_movie_posters[1])
-
-    with col3:
-        st.text(recommended_movie_names[2])
-        st.image(recommended_movie_posters[2])
-    with col4:
-        st.text(recommended_movie_names[3])
-        st.image(recommended_movie_posters[3])
-    with col5:
-        st.text(recommended_movie_names[4])
-        st.image(recommended_movie_posters[4])
-
-
-
-
+    names, posters, overviews = recommend(selected_movie)
+    cols = st.columns(5)
+    
+    for i, col in enumerate(cols):
+        with col:
+            st.text(names[i])
+            st.image(posters[i])
+            with st.expander("Description"):
+                st.write(overviews[i])
